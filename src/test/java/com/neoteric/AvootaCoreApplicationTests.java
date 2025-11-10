cf5364019aa4a57810be8e3473ab91c9690f89ba
manjith677 <manjith9989@gmail.com>
2025-11-10 12:38:08 +0530

    core plainproject

---
+++ src/test/java/com/neoteric/AvootaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoteric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvootaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
